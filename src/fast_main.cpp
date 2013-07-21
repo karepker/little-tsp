b87c4505dcb8b0756996bdadafbbf2a76769d36d
Kar Epker <karepker+services@gmail.com>
2013-07-20 18:14:21 -0700

    Made different main files for (future) different executables

---
+++ src/fast_main.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////
+// Kar Epker's implementation of a greedy heuristic for the TSP
+////////////////////////////////////////////////////////////////////////////////
+
+// Project
+#include "graph.hpp"
+
+using std::cout;
+
+int main()
+{
+	// construct the Graph, find the path using the naive TSP algorithm
+	Graph g;
+	Path p = g.fastTSP();
+	cout << p;
+	return 0;
+}
